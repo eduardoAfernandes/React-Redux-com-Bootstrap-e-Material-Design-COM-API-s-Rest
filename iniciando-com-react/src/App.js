bfad9444a41fdfbc0bb59f395f4f86b3bd9cf190
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2020-11-23 13:29:30 -0300

    Adicionando comportamento a um estado

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -3,12 +3,22 @@ import React from 'react';
 class App extends React.Component {
 
   state = {
-    nome : 'Eduardo'
+    nome : ''
+  }
+
+  modificaNome = (event) => {
+    let nome = event.target.value;
+    
+    //Atualizando o valor do state dinamicamente pegando o valor digitado no input
+    this.setState({
+      nome: nome
+    })
   }
 
   render(){
     return(
       <>
+      <input type="text" placeholder="Digite aqui" onChange={this.modificaNome}  />
       <h1>Hello {this.state.nome} </h1>
       </>
     )
